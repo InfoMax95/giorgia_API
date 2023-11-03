b0d156f491af49c9f9a5bbb448bafbd3fcbbd139
Max Gasaro <massimilianogasaro@me.com>
2023-11-03 11:37:57 +0100

    create first ejs content

--- routes/blog.js
+++ routes/blog.js
@@ -2,15 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/articoli", (req, res) => {
-    res.status(301).redirect("/blog/news");
+    const articoli = [
+        { titolo: "titolo articolo 1", testo: "testo articolo 1" },
+        { titolo: "titolo articolo 2", testo: "testo articolo 2" },
+        { titolo: "titolo articolo 3", testo: "testo articolo 3" }
+    ];
+    res.render('articoli', {
+        title: "articoli del blog",
+        elencoArticoli: articoli
+    });
+    // res.status(301).redirect("/blog/news"); esempio di redirect
 });
 
 router.get("/news", (req, res) => {
-    res.send("Pagina articoli")
+    res.send("Pagina articoli");
 });
 
 router.get("/articolo/:titolo", (req, res) => {
-    res.send("Pagina articolo")
+    res.send("Pagina articolo");
 });
 
 module.exports = router;
